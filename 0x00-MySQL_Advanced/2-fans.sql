7ae1cb04ffa57831d071fab12281a9299876eab9
Andee WSL <andeeolayinkascott@gmail.com>
2023-12-14 02:40:22 +0100

    Task 2

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,16 @@
 -- This script ranks country origins of bands,
 -- ordered by the number of (non-unique) fans
 
-SELECT origin, fans AS nb_fans FROM metal_bands ORDER BY fans DESC;
+DROP FUNCTION IF EXISTS fans;
+
+DELIMITER //
+CREATE FUNCTION fans(orig VARCHAR(255))
+RETURNS INTEGER DETERMINISTIC
+BEGIN
+    DECLARE total_fans INTEGER;
+    SELECT SUM(fans) INTO total_fans FROM metal_bands WHERE origin = orig;
+    RETURN total_fans;
+END //
+DELIMITER ;
+
+SELECT origin, fans(origin) AS nb_fans FROM metal_bands GROUP BY origin ORDER BY nb_fans DESC;
