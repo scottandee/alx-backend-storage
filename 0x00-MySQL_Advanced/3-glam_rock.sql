f6fe430d1ed1b420a38c74b22302c737f76e4753
Andee WSL <andeeolayinkascott@gmail.com>
2023-12-14 02:48:39 +0100

    Task 3 fix

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,8 +2,8 @@
 -- as their main style, ranked by their longevity
 
 DROP FUNCTION IF EXISTS lifespan;
-DELIMITER //
 
+DELIMITER //
 CREATE FUNCTION lifespan(formed INTEGER, split INTEGER)
     RETURNS INTEGER DETERMINISTIC 
 BEGIN
@@ -13,10 +13,9 @@ BEGIN
         RETURN split - formed;
     END IF;
 END//
-
 DELIMITER ;
 
-SELECT band_name, lifespan(split, formed) AS lifespan
+SELECT band_name, lifespan(formed, split) AS lifespan
 FROM metal_bands
-WHERE metal_bands.style = 'Glam rock'
-ORDER BY lifespan;
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
