ff568098872a49e9eaaff43b04ff702710ce5197
Andee WSL <andeeolayinkascott@gmail.com>
2023-12-21 16:17:52 +0100

    Task 10, 11 fix

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -9,4 +9,4 @@ def update_topics(mongo_collection, name, topics):
     """This function changes all topics of a school
     document based on the name
     """
-    mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -4,12 +4,14 @@ returns a list of schools that have a specific
 topic
 """
 
+from typing import List
+
 
 def schools_by_topic(mongo_collection, topic):
     """This function returns a list of schools
     that have a specific topic
     """
-    result = []
+    result: List = []
     schools = mongo_collection.find()
     for sch in schools:
         if sch.get("topics"):
